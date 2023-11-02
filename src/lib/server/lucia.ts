7fabfaa2c7e965729d6cd424f065c4cc8f44cd27
Roy <roywashington1547@gmail.com>
2023-11-02 18:53:13 -0500

    stuff

---
+++ src/lib/server/lucia.ts
@@ -0,0 +1,12 @@
+// import { lucia } from 'lucia';
+// import { sveltekit } from 'lucia/middleware';
+// import { dev } from '$app/env';
+// import
+
+// export const auth = lucia({
+// 	env: dev ? 'DEV' : 'PROD',
+// 	middleware: sveltekit(),
+//     adapter:
+// });
+
+// export type Auth = typeof auth;
